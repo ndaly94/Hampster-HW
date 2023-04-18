2b0c42a68e2104939b4a9d83e4788b9100afdbfc
NDaly94 <NDaly94@gmail.com>
2023-04-18 13:04:24 -0400

    v4

--- code/dom-practice/js/main.js
+++ code/dom-practice/js/main.js
@@ -1,6 +1,6 @@
 const titleEl = document.getElementById('title');
 console.log(titleEl);
-const pEl = document.querySelector('cool');
+const pEl = document.getElementById('cool');
 console.log(pEl);
 titleEl.style.textAlign = 'center';
 pEl.style.color = 'purple';
